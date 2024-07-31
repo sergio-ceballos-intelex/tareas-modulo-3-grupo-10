efea41752eed9287d4fdb27ffbcf7e5ae020f229
JorgeArenas85 <jorgeaarenas85@gmail.com>
2024-07-30 19:54:28 -0500

    Adjunto segundo punto de la tarea 3

--- Jorge-Arenas-3204119246/Jorge-Arenas-3204119246-tarea-3/tarea.js
+++ Jorge-Arenas-3204119246/Jorge-Arenas-3204119246-tarea-3/tarea.js
@@ -1,3 +1,10 @@
-for (let i = 1; i <50; i++) {
-    console.log (1); 
+//Punto N°1
+//for (let i = 1; i <=50; i++) {
+    //console.log (i); 
+//}
+
+
+//Punto °2
+for (let i = 1; i <=2000; i++) {
+    console.log (i);
 }
