9c503d23fd777f628336a47cf1e6a4bde84cea23
harri <harrybalanta@gmail.com>
2024-07-30 23:06:21 -0500

    se modifica programa que sumaba numeros del 1 al 200 para que ahora sume solo los numeros que inician  por 2

--- Harrinson-Balanta-3137735896/Tarea-3/main.js
+++ Harrinson-Balanta-3137735896/Tarea-3/main.js
@@ -1,4 +1,16 @@
+// // programa que suma los numeros del 1 al 50
+// for (let num =1; num <=2000; num ++){
+//     console.log(num,(" "))
+//   }
 
-for (let num =1; num <=2000; num ++){
-  console.log(num,(" "))
+// // programa que suma los numeros del 1 al 200
+// for (let num =1; num <=2000; num ++){
+//   console.log(num,(" "))
+// }
+
+for (let nume = 2; nume <= 2000; nume++){
+    let ope = nume.toString();
+    if (ope.startsWith('2')){
+        console.log(ope)
+    };
 }
