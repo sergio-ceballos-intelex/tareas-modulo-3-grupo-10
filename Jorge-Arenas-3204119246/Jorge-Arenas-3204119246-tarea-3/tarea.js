2def244d20f10fce0153602938c38091a4bb086f
JorgeArenas85 <jorgeaarenas85@gmail.com>
2024-07-30 20:05:26 -0500

    corrigo punto 2 de la tarea

--- Jorge-Arenas-3204119246/Jorge-Arenas-3204119246-tarea-3/tarea.js
+++ Jorge-Arenas-3204119246/Jorge-Arenas-3204119246-tarea-3/tarea.js
@@ -5,6 +5,13 @@
 
 
 //Punto °2
-for (let i = 1; i <=2000; i++) {
-    console.log (i);
-}
+//for (let i = 1; i <=2000; i++) {
+   // console.log (i);
+//}
+
+//Punto N°3
+for (let i = 1; i <= 2000; i++) {
+    if (i.toString().startsWith('2')) {
+      console.log(i);
+    }
+  }
