45c3d00822c0515467a14071e658c8f03259908a
danielamendoza <inadmendoza1996@gmail.com>
2024-07-31 17:15:28 -0500

    subo segunda tarea

--- Daniela_Mendoza_3134260236/tarea-3/main.js
+++ Daniela_Mendoza_3134260236/tarea-3/main.js
@@ -1,6 +1,14 @@
+// let myArray =[]
+
+// for(let contador = 1; contador <= 50; contador++){
+//     myArray.push(contador);
+// }
+// console.log(myArray);
+
+
 let myArray =[]
 
-for(let contador = 1; contador <= 50; contador++){
+for(let contador = 1; contador <= 2000; contador++){
     myArray.push(contador);
 }
 console.log(myArray);
