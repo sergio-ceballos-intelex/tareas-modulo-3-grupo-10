92f71cf24450ff6393d816b2ed71616453e0607b
afsoftwaresolutions <felipe.pizarro.betancurt@gmail.com>
2024-07-31 09:23:04 -0500

    new console log added to print numbers from 1 to 2000

--- andres-pizarro-3054133601/Tarea 3/tarea3.js
+++ andres-pizarro-3054133601/Tarea 3/tarea3.js
@@ -2,6 +2,8 @@ function generateRange(start, end) {
     return Array.from({length: end - start + 1 }, (_, i) => start + i);
 }
 
-const numbers = generateRange(1, 50);
+let numbers = generateRange(1, 50);
+console.log(numbers);
 
+numbers = generateRange(1, 2000);
 console.log(numbers);
