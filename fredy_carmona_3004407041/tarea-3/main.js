ac53e676a785ece65b3efc09e2b3e75f526266ed
fredy carmona <fredycar2210@gmail.com>
2024-07-31 17:53:15 -0500

    subo primera tarea

---
+++ fredy_carmona_3004407041/tarea-3/main.js
@@ -0,0 +1,25 @@
+let myArray =[]
+
+for(let contador = 1; contador <= 50; contador++){
+    myArray.push(contador);
+}
+console.log(myArray);
+
+
+// let myArray =[]
+
+// for(let contador = 1; contador <= 2000; contador++){
+//     myArray.push(contador);
+// }
+// console.log(myArray);
+
+
+// // Lista de números de ejemplo
+// const numeros = [10, 20, 25, 30, 42, 55, 21, 22, 100, 200, 250];
+
+// // Filtrar y mostrar solo los números que empiezan con '2'
+// numeros.forEach(numero => {
+//     if (numero.toString().startsWith('2')) {
+//         console.log(numero);
+//     }
+// });
