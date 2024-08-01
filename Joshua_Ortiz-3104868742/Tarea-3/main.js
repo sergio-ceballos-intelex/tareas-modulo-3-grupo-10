8f27a57f47ccc3dbfca0c4d0b75cabaa5ddb73ec
Joshua <jortiz336@misena.edu.co>
2024-07-31 23:44:04 -0700

    se creo script que muestre en consola los numeros del 1 al 2000 pero convirtiendolos a tipo de dato string y solamente imprimiendo los que empiezan en dos

--- Joshua_Ortiz-3104868742/Tarea-3/main.js
+++ Joshua_Ortiz-3104868742/Tarea-3/main.js
@@ -1,6 +1,20 @@
 
-for (let Contador = 0; Contador <= 2000; Contador++){
-    console.log(Contador);
+// for (let Contador = 0; Contador <= 2000; Contador++){
+//     console.log(Contador);
+// }
+
+
+
+
+
+for (let Inicia = 0; Inicia <= 2000; Inicia++){
+    if (Inicia.toString().startsWith('2')){console.log(Inicia);}
+    
 }
 
 
+
+// for (let Contador = 0; Contador <= 2000; Contador++){
+//     if (Contador % 2 === 0){console.log(Contador);}
+    
+// }
