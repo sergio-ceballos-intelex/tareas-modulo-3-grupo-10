e14617385b91b57041c46c123d4bdb92a8272ada
Daniel-Cardenas <danielcar_777@hotmail.es>
2024-07-31 20:06:16 -0500

    Segundo commit, numeros del 1 al 2000

--- Daniel-Cardenas-3196083047-Tarea3/Tarea3.js
+++ Daniel-Cardenas-3196083047-Tarea3/Tarea3.js
@@ -1,6 +1,11 @@
 // Punto 1
-for(let i = 1; i <= 50; i++){
-    document.write(i,'<br>'); 
-} 
+// for(let i = 1; i <= 50; i++){
+//     document.write(i,'<br>'); 
+// } 
 
 
+
+// Punto 2
+for (let a = 1; a <= 2000; a++){
+    console.log(a, "\n");
+}
