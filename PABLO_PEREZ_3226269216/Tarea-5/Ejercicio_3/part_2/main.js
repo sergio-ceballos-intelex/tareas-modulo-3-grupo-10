e2f0779fa679fb67273fffa06e3b8d85f02abd91
Pablo24 <vtrpablo4@gmail.com>
2024-08-06 20:54:23 -0500

    tarea 5, parte 3, ejercicios boleanos

---
+++ PABLO_PEREZ_3226269216/Tarea-5/Ejercicio_3/part_2/main.js
@@ -0,0 +1,18 @@
+//true && (false || false && (true && (true || false && true))) || false && (true || false)
+console.log(false && true)
+//true && (false || false && (true && (true || false))) || false && (true || false)
+console.log(true || false)
+//true && (false || false && (true && true)) || false && (true || false)
+console.log(true && true)
+//true && (false || false && true) || false && (true || false)
+console.log(false && true)
+//true && (false || false ) || false && (true || false)
+console.log(false || false )
+//true && false || false && (true || false)
+console.log(true || false )
+//true && false || false && true
+console.log(true && false )
+//false || false && true
+console.log(false && true )
+//false || false
+console.log(false || false )
