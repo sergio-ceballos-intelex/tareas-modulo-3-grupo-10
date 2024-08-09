1d9e3b5ec1881177d87394e54437de8b85d5c333
Pablo24 <vtrpablo4@gmail.com>
2024-08-08 19:21:26 -0500

    Tarea_6, ejercicio 5, se corrigue codigo

--- PABLO_PEREZ_3226269216/Tarea-6/Ejercicio_5/main.js
+++ PABLO_PEREZ_3226269216/Tarea-6/Ejercicio_5/main.js
@@ -1,12 +1,8 @@
 //Utiliza un bucle for para concatenar frutas2 al final de frutas1. Luego, imprime el array resultante.
 let frutas1 = ["Manzana", "Plátano", "Naranja"];
 let frutas2 = ["Fresa", "Uva", "Piña"];
-let TotalFrutas = []
 
-for (num= 0; num < frutas1.length ; num ++){
-    TotalFrutas.push(frutas1[num])
-}
 for (num= 0; num < frutas2.length ; num ++){
-    TotalFrutas.push(frutas2[num])
+    frutas1.push(frutas2[num])
 }
-console.log(TotalFrutas)
+console.log(frutas1)
