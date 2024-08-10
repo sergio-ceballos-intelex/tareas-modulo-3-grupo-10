6a7b76c9949b68766d8f6dec968cd67c11394e5d
Yuly Carolina Mendoza Quiroga <yulibetquiroga@gamil.com>
2024-08-09 22:44:01 -0300

    Realizo entrega de punto 1 tarea 3

---
+++ Carolina_Mendoza_93873657580/Tarea_3/main.js
@@ -0,0 +1,9 @@
+//1.Crear un archivo .js y desarrollar un script
+//que muestre en consola los numeros del 1 al 50.
+
+let myArray =[]
+
+for(let contar = 1; contar <= 50; contar++){
+    myArray.push(contar);
+}
+console.log(myArray);
