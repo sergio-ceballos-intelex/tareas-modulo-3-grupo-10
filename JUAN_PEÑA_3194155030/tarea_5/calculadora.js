25d50d905bb703f68bb241155fc0491c8457371e
Juan Camilo Peña <juan1000352504@gmail.com>
2024-08-12 11:36:35 -0500

    corrección linea 16

--- JUAN_PEÑA_3194155030/tarea_5/calculadora.js
+++ JUAN_PEÑA_3194155030/tarea_5/calculadora.js
@@ -1,22 +1,26 @@
-const numero1=Number(prompt("ingrese un número "))
-const numero2=Number(prompt("ingrese un segundo número"))
-const operacion=prompt("Operación que deseas realizar : + , - , * , / ").toLowerCase();
+const numero1 = Number(prompt("ingrese un número "));
+const numero2 = Number(prompt("ingrese un segundo número"));
+const operacion = prompt("Operación que deseas realizar : +, -, * /").toLowerCase();
 
 let resultado;
 
-if(operacion==="+"){
+if (operacion === "+") {
     resultado = numero1 + numero2;
-} else if (operacion==="-"){
+} else if (operacion === "-") {
     resultado = numero1 - numero2;
-} else if (operacion==="/"){
+} else if (operacion === "/") {
     resultado = numero1 / numero2;
-}else if (operacion==="*"){
+} else if (operacion === "*") {
     resultado = numero1 * numero2;
 } else {
-    resultado=("operación no conocida")
+    resultado = "operación no conocida";
 }
 
 document.write("El resultado de la operación es: " + resultado);
+
+
+
+
 
 
 
