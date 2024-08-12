a86b0d76e650efcadd6287d526efd84a03e8046d
Melissa Gómez <melissagomezhoyos02@gmail.com>
2024-08-11 19:16:26 -0500

    corrijo tarea 3 imprimiendo solo los números que empiezan por 2, desde el 1 al 2000

--- MELISSA-GÓMEZ-3193962490/Tarea_3/main.js
+++ MELISSA-GÓMEZ-3193962490/Tarea_3/main.js
@@ -1,3 +1,12 @@
 for (let i = 1; i <= 2000; i++) {
-    console.log(i);
+    if (i.toString().startsWith("2")) {
+        console.log(i);
+    }
 }
+
+// Explicación:
+// Bucle for: Recorre los números del 1 al 2000.
+// i.toString(): Convierte el número actual a una cadena para poder usar startsWith().
+// startsWith("2"): Verifica si la cadena comienza con "2".
+// console.log(i): Imprime el número si cumple con la condición.
+
