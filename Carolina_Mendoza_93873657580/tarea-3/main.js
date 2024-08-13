c7ed9181a6570e03d0e4f562ad2cb2a2ea5dc617
Yuly Carolina Mendoza Quiroga <yulibetquiroga@gamil.com>
2024-08-13 18:52:48 -0300

    punto 2 tarea 3

--- Carolina_Mendoza_93873657580/tarea-3/main.js
+++ Carolina_Mendoza_93873657580/tarea-3/main.js
@@ -1,8 +1,16 @@
 // 1.Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
 
+// let myArray =[]
+
+// for(let contar = 1; contar <= 50; contar++){
+//     myArray.push(contar);
+// }
+// console.log(myArray);
+// 2.Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
+
 let myArray =[]
 
-for(let contar = 1; contar <= 50; contar++){
+for(let contar = 1; contar <= 2000; contar++){
     myArray.push(contar);
 }
 console.log(myArray);
