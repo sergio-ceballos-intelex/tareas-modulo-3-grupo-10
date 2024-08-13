e4e0415ea44778217d726ded732b02862dd26229
Yuly Carolina Mendoza Quiroga <yulibetquiroga@gamil.com>
2024-08-13 18:55:35 -0300

    punto 3 tarea 3

--- Carolina_Mendoza_93873657580/tarea-3/main.js
+++ Carolina_Mendoza_93873657580/tarea-3/main.js
@@ -8,9 +8,17 @@
 // console.log(myArray);
 // 2.Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
 
-let myArray =[]
+// let myArray =[]
 
-for(let contar = 1; contar <= 2000; contar++){
-    myArray.push(contar);
-}
-console.log(myArray);
+// for(let contar = 1; contar <= 2000; contar++){
+//     myArray.push(contar);
+// }
+// console.log(myArray);
+//3.Modificar el script para que ahora imprima solo los numeros que empiezan por 2 (convertir a string el numero y verificar con starsWith).
+const numeros = [22, 20, 25, 30, 47, 35, 28, 72, 15, 17, 50];
+
+numeros.forEach(numero => {
+    if (numero.toString().startsWith('2')) {
+        console.log(numero);
+    }
+});
