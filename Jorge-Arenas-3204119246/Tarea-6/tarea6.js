d60f3c5b9e4a26c9dc8dca8a376216759fc42dbc
JorgeArenas85 <jorgeaarenas85@gmail.com>
2024-08-14 16:28:07 -0500

    realizado el cambio

---
+++ Jorge-Arenas-3204119246/Tarea-6/tarea6.js
@@ -0,0 +1,72 @@
+//Punto 1
+
+//let number = [1, 2, 3, 4, 5];
+// let result = [];
+
+// for (let i = 0; i < number.length; i++) {
+//     result.push(number[i] * 2);
+// }
+// console.log(result);
+
+// Punto 2
+
+// let productos = [
+//     { nombre: "Camisa", precio: 20 },
+//     { nombre: "Pantalón", precio: 30 },
+//     { nombre: "Zapatos", precio: 50 },
+// ];
+// let productoMasBarato = productos[0]; 
+
+// for (let i = 1; i < productos.length; i++) {
+//     if (productos[i].precio < productoMasBarato.precio) {
+//         productoMasBarato = productos[i]; 
+//     }
+// }
+// console.log(`Producto más barato: ${productoMasBarato.nombre}, Precio: $${productoMasBarato.precio}`);
+
+//Punto 3
+
+// let estudiantes = [
+//     { nombre: "Juan", edad: 21 },
+//     { nombre: "María", edad: 22 },
+//     { nombre: "Pedro", edad: 19 },
+//   ];
+//   for (let i = 0; i <estudiantes.length; i++) {
+//     estudiantes [i].edad += 1;
+//   }
+//   console.log (estudiantes);
+
+//Punto 4
+// let libros = [
+//     { titulo: "El principito", autor: "Antoine de Saint-Exupéry" },
+//     { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+//     { titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
+//   ];
+// let palabraQueEmpiezaPorEl = [];
+
+// for (let i = 0; i < libros.length; i++) {
+//     if (libros[i].titulo.includes("El")) {
+//         palabraQueEmpiezaPorEl.push(libros[i]);
+//     }
+// }
+// console.log (palabraQueEmpiezaPorEl);
+
+//Punto 5
+
+// let frutas1 = ["Manzana", "Plátano", "Naranja"];
+// let frutas2 = ["Fresa", "Uva", "Piña"];
+// for (let i = 0; i <frutas2.length; i++){
+//     frutas1.push(frutas2[i]);
+// }
+// console.log (frutas1);
+
+
+
+
+
+
+
+
+
+
+
