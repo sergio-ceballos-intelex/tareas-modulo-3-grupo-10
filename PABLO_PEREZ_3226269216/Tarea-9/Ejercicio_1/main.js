e189e71424ea1020f3b0fcd2e587bc836ff81d7d
Pablo24 <vtrpablo4@gmail.com>
2024-08-15 19:22:03 -0500

    Tarea 9, Ejercicio 2, uso del filter con callback

--- PABLO_PEREZ_3226269216/Tarea-9/Ejercicio_1/main.js
+++ PABLO_PEREZ_3226269216/Tarea-9/Ejercicio_1/main.js
@@ -3,10 +3,10 @@
 //Ejercicio:
 
 function sumarArreglo(numeros, callback) {
-  const suma = numeros.reduce((acumulado, num) => acumulado + num,0);
-  callback(suma)
-}
-const numeros = [1, 2, 3, 4, 5];
-sumarArreglo(numeros, function (resultado) {
-  console.log(resultado); 
-});
+    const suma = numeros.reduce((acumulado, num) => acumulado + num,0);
+    callback(suma)
+  }
+  const numeros = [1, 2, 3, 4, 5];
+  sumarArreglo(numeros, function (resultado) {
+    console.log(resultado); 
+  });
