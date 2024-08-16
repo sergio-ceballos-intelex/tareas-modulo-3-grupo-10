1b8fe17a101aabae0761bfa9cee864574c670aab
Pablo24 <vtrpablo4@gmail.com>
2024-08-15 19:08:42 -0500

    Tarea-9, ejercicio 1, uso del callback

---
+++ PABLO_PEREZ_3226269216/Tarea-9/Ejercicios/Ejercicio_1/main.js
@@ -0,0 +1,12 @@
+//Callbacks en Manejo de Datos: 
+//Descripción: Crea una función que tome un arreglo de números y un callback, y llame al callback con el resultado de sumar todos los números del arreglo.
+//Ejercicio:
+
+function sumarArreglo(numeros, callback) {
+  const suma = numeros.reduce((acumulado, num) => acumulado + num,0);
+  callback(suma)
+}
+const numeros = [1, 2, 3, 4, 5];
+sumarArreglo(numeros, function (resultado) {
+  console.log(resultado); 
+});
