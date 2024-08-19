2535f58b1b1b6662c5d5d38e442f86a6d8c1cb02
Julieth <july95march@gmail.com>
2024-08-19 11:52:30 -0500

    subo tarea 7 con sus 4 puntos

---
+++ Julieth_Mosquera_3126043870/tarea-7/punto-2/estudiantes.js
@@ -0,0 +1,10 @@
+const estudiantes = [
+    { nombre: "Camilo", edad: 33, promedio: 10 },
+    { nombre: "Juan", edad: 30, promedio: 6 },
+    { nombre: "Andres", edad: 29, promedio: 8.7 },
+];
+
+console.log("El nombre del segundo estudiante es:", estudiantes[1].nombre);
+
+estudiantes[0].edad = 25;
+console.log("el primer estudiante es:", estudiantes[0]);
