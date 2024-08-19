91287e86d84df185bf2bbc2a74af03dfb83d15b4
Juan Camilo Peña <juan1000352504@gmail.com>
2024-08-19 01:56:00 -0500

    Elimina marcadores de conflicto y limpia el archivo punto1.js

--- JUAN_PEÑA_3194155030/tarea_6/punto1.js
+++ JUAN_PEÑA_3194155030/tarea_6/punto1.js
@@ -5,11 +5,7 @@ for (let i=0; i<numeros.length; i++){
     const multiplicador= 2;
     const multiplicando = numeros [i]
     const operacion= multiplicando * multiplicador
-    resultados.push(operacion)
+    resultados.push(operacion
 }
 
-<<<<<<< HEAD
-console.log(resultados)
-=======
-console.log(resultados)
->>>>>>> 46ace9be7393f2174fc4227255d955a05321f316
+
